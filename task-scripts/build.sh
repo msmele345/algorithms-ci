b1569f1ba789912da85040d16117c857b0e47af1
Mitch Mele <mitch@bcbsil.com>
2020-03-09 07:25:32 -0500

    Add task files and basic run scripts

---
+++ task-scripts/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+pushd algorithm-api-git/
+
+./gradlew clean
+#cp -r algorithm-api-git/application/build/libs/* algorithm-api-jar/
+#cp -r algorithm-api-git/application/build/libs/* ../build/algorithm-api-jar/
+
+popd
