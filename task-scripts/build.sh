f670c659abcaff114c7cabdb8f2ec39ced75e5d4
Mitch Mele <mitch@bcbsil.com>
2020-03-09 08:07:01 -0500

    Update task with new task scripts

--- task-scripts/build.sh
+++ task-scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
+set -e
 pushd algorithm-api-git/
 
-./gradlew clean
+./gradlew clean test
 #cp -r algorithm-api-git/application/build/libs/* algorithm-api-jar/
 #cp -r algorithm-api-git/application/build/libs/* ../build/algorithm-api-jar/
 
