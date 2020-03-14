7d36324de54405afc6bdf2ea493dd18f691ee5be
Mitch Mele <mitch@bcbsil.com>
2020-03-14 09:53:47 -0500

    Update gitignore for new files

--- task-scripts/build.sh
+++ task-scripts/build.sh
@@ -4,6 +4,6 @@ cd algorithm-api-git/
 
 ./gradlew clean test
 #cp -r algorithm-api-git/application/build/libs/* algorithm-api-jar/
-cp -r algorithm-api-git/application/build/libs/* ../build/algorithm-api-jar/
+#cp -r algorithm-api-git/application/build/libs/* ../build/algorithm-api-jar/
 
 #popd
