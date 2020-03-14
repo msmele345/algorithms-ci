22c9a052b67aab177fee89c3718887fe6f352197
Mitch Mele <mitch@bcbsil.com>
2020-03-14 10:38:37 -0500

    Update version job

--- task-scripts/build.sh
+++ task-scripts/build.sh
@@ -7,8 +7,8 @@ mkdir ../jars/stubs
 
 #./gradlew clean test
 ./gradlew clean build
-cp -r algorithm-api-git/application/build/libs/* ../jars/artifact
-cp -r algorithm-api-git/application/build/libs/* ../jars/stubs
+cp -r algorithm-api-git/build/libs/* ../jars/artifact
+cp -r algorithm-api-git/build/libs/* ../jars/stubs
 #cp -r algorithm-api-git/application/build/libs/* ../build/algorithm-api-jar/
 
 #popd
