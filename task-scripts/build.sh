f7aa8967da703046b5a1127a5acffc67a4c78cec
Mitch Mele <mitch@bcbsil.com>
2020-03-14 11:39:11 -0500

    Update pipeline

--- task-scripts/build.sh
+++ task-scripts/build.sh
@@ -8,6 +8,9 @@ mkdir ../jars/artifact
 #./gradlew clean test
 ./gradlew build
 cp /tmp/build/80754af9/algorithm-api-git/build/libs/*.jar ../jars/artifact/algorithm-api.jar
+
+echo JAR FILE
+ls ../jars/artifact
 #cp target/*.jar ../build/algorithm-api.jar
 #cp -r algorithm-api-git/application/build/libs/* ../build/algorithm-api-jar/
 
